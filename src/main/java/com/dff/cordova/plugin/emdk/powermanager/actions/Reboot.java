c2b44f8bedcd3dfd126518d50bc3a4d32dfd3fd6
SebastianKnabe <sebastian.knabe@circlon.de>
2019-10-01 13:59:49 +0200

    feat(reboot): action to reboot the device
    this action restarts the device.

---
+++ src/main/java/com/dff/cordova/plugin/emdk/powermanager/actions/Reboot.java
@@ -0,0 +1,64 @@
+package com.dff.cordova.plugin.emdk.powermanager.actions;
+
+import android.util.Xml;
+
+import com.dff.cordova.plugin.emdk.powermanager.helpers.ProfileManagerHelper;
+import com.symbol.emdk.EMDKManager;
+import com.symbol.emdk.EMDKResults;
+import com.symbol.emdk.ProfileManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.StringReader;
+
+import javax.inject.Inject;
+
+public class Reboot extends PluginAction {
+    public static final String ACTION = "reboot";
+    private String profileName = "PowerManagerProfile";
+    private ProfileManagerHelper profileManager;
+    private EMDKManager emdkManager = null;
+    
+    // Contains the parm-error name (sub-feature that has error)
+    private String errorName = "";
+    
+    // Contains the characteristic-error type (Root feature that has error)
+    private String errorType = "";
+    
+    // contains the error description for parm or characteristic error.
+    private String errorDescription = "";
+    
+    @Inject
+    public Reboot(ProfileManagerHelper profileManager){
+        this.profileManager = profileManager;
+    }
+    
+    @Override
+    protected void execute() throws Exception {
+        String[] modifyData = new String[1];
+        modifyData[0] = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+            + "<characteristic type=\"Profile\">"
+            + "<parm name=\"ProfileName\" value=\"PowerManagerProfile\"/>"
+            + "<characteristic type=\"PowerMgr\">"
+            + "<parm name=\"ResetAction\" value=\"" + 4 + "\"/>"
+            + "</characteristic>"
+            + "</characteristic>";
+    
+        JSONObject jsonResult = profileManager.processProfile(profileName,
+                                                            ProfileManager.PROFILE_FLAG.SET, modifyData);
+        if(jsonResult == null) {
+            mCallbackContext.error("result is null");
+        } else {
+            mCallbackContext.success(jsonResult);
+        }
+        
+    }
+    
+    @Override
+    public String getActionName() {
+        return ACTION;
+    }
+}
